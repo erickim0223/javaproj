d1f649db876c29359d96f50dc449f131140f7bf1
erickim0223 <erickim0223@yahoo.com>
2023-11-13 20:43:43 -0500

    comit progra

---
+++ hello/src/test/java/com/eric/hello/HelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eric.hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
